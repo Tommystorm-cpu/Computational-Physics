50760bcfd45ed5e7285d4ec892bd455f83e80e1b
Tommy <tommykdavey@gmail.com>
2023-06-20 21:05:59 +0100

    Minor Fixes
    Changed a few incorrect values in PlanetData, and adjusted Task 2 to use it.

--- Task Files/Task 1/Task 1.py
+++ Task Files/Task 1/Task 1.py
@@ -1,21 +1,23 @@
-import matplotlib.pyplot as plt  #imports modules
+import matplotlib.pyplot as plt  # imports modules
 import numpy as np
 import PlanetData as Pd
-plt.ylabel("T/Yr")  #labels axis
+plt.ylabel("T/Yr")  # labels axis
 plt.xlabel("(a/AU)^(3/2)")
 
-y = [] # data set for y axis
-x = []   #data set for x axis
-size = len(Pd.object_data) #number of entries in object_data library
+y = []  # data set for y axis
+x = []   # data set for x axis
+size = len(Pd.object_data)  # number of entries in object_data library
 
 for a in range(size):
-    x.append(float(Pd.object_data[a][1]) ** 1.5) #computes data for the x axis
+    x.append(float(Pd.object_data[a][1]) ** 1.5)  # computes data for the x axis
     y.append(float(Pd.object_data[a][6]))
 
-m, c = np.polyfit(x, y, 1)   #computes line of best fit in the form y = mx + c
-plt.axline(xy1=(0, c), slope=m, color="r", label=f'$ y = {m}x$') #plots line of best fit and assigns gradient as legend
-plt.axis([0, x[size-1]*1.1, 0, y[size-1]*1.1]) #defines limits on axis
-plt.plot(x, y, "bs") #plots data as points 
+line = np.polyfit(x, y, 1)   # computes line of best fit in the form y = mx + c
+m = line[0]
+c = line[1]
+plt.axline(xy1=(0, c), slope=m, color="r", label=f'$ y = {m}x$')  # plots line of best fit, assigns gradient as legend
+plt.axis([0, x[size-1]*1.1, 0, y[size-1]*1.1])  # defines limits on axis
+plt.plot(x, y, "bs")  # plots data as points
 
 plt.legend(loc="upper right")
 plt.show()
--- Task Files/Task 2/Task_2_Web.py
+++ Task Files/Task 2/Task_2_Web.py
@@ -1,31 +1,24 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import math
-
-# alpha, eccentricity, name
-inner_planets = {
-    0: np.array([0.387, 0.21, "Mercury"]),
-    1: np.array([0.712, 0.01, "Venus"]),
-    2: np.array([1, 0.02, "Earth"]),
-    3: np.array([1.523, 0.09, "Mars"])
-}
+import PlanetData as Pd
 
 fig = plt.figure()
 ax = fig.add_subplot()
 ax.set_aspect('equal', adjustable='box')
 
-for i in range(len(inner_planets)):
+for i in range(len(Pd.inner_planets)):
     x_coords = []
     y_coords = []
     for theta in np.arange(0, (2*math.pi)+1, 0.1):
-        alpha = float(inner_planets[i][0])
-        eccen = float(inner_planets[i][1])
-        radius = (alpha*(1-(eccen*eccen)))/(1-eccen*math.cos(theta))
+        semi_major = float(Pd.inner_planets[i][1])
+        eccen = float(Pd.inner_planets[i][2])
+        radius = (semi_major*(1-(eccen*eccen)))/(1-eccen*math.cos(theta))
         x = radius * math.cos(theta)
         y = radius * math.sin(theta)
         x_coords.append(x)
         y_coords.append(y)
-    plt.plot(x_coords, y_coords, label=inner_planets[i][2])
+    plt.plot(x_coords, y_coords, label=Pd.inner_planets[i][7])
 
 plt.plot(0, 0, marker="o", markersize=20, markerfacecolor="yellow")
 plt.title("Inner Solar System")
