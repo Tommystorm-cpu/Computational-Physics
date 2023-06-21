82721a7106c622a43ac35d815d95a0811c8e4712
Tommy <tommykdavey@gmail.com>
2023-06-21 23:47:00 +0100

    Task 3 Animation Done
    Cleanup tasks remain

--- Task Files/Task 3/Task 3.py
+++ Task Files/Task 3/Task 3.py
@@ -8,18 +8,18 @@ fig = plt.figure()
 ax = fig.add_subplot()
 ax.set_aspect('equal', adjustable='box')
 
-for i in range(len(Pd.inner_planets)):
+for i in range(len(Pd.outer_planets)):
     x_coords = []
     y_coords = []
     for theta in np.arange(0, (2*math.pi)+1, 0.1):
-        semi_major = float(Pd.inner_planets[i][1])
-        eccen = float(Pd.inner_planets[i][2])
+        semi_major = float(Pd.outer_planets[i][1])
+        eccen = float(Pd.outer_planets[i][2])
         radius = (semi_major*(1-(eccen*eccen)))/(1-eccen*math.cos(theta))
         x = radius * math.cos(theta)
         y = radius * math.sin(theta)
         x_coords.append(x)
         y_coords.append(y)
-    plt.plot(x_coords, y_coords, label=Pd.inner_planets[i][7])
+    plt.plot(x_coords, y_coords, label=Pd.outer_planets[i][7])
 
 
 class Planet:
@@ -27,12 +27,11 @@ class Planet:
         self.period = float(data[6])
         self.semi_major = float(data[1])
         self.eccen = float(data[2])
-        self.colour = (1, 0, 0)
 
-        self.point, _ = plt.plot(0, 0, 0, 'o', markersize=7, markerfacecolor=self.colour,
-                                      markeredgecolor=self.colour)
+        self.point, = ax.plot(0, 0, 'o', markersize=7)
 
     def step_function(self, time):
+        time = time / 10
         theta = (2 * math.pi * time) / self.period
         r = (self.semi_major*(1-self.eccen**2)) / (1 - (self.eccen * math.cos(theta)))
         x = r * math.cos(theta)
@@ -48,9 +47,9 @@ plt.xlabel("X (AU)")
 plt.ylabel("Y (AU)")
 plt.legend(loc="upper right")
 
-mercury = Planet(Pd.inner_planets[0])
-frame_count = int(round((360 * mercury.period) / (2 * math.pi)))
-
-anim = FuncAnimation(fig, mercury.step_function, frames=frame_count, interval=1, repeat=True)
+ani_list = []
+for planet in Pd.outer_planets:
+    plan_class = Planet(Pd.outer_planets[planet])
+    ani_list.append(FuncAnimation(fig, plan_class.step_function, repeat=True, interval=1))
 
 plt.show()
