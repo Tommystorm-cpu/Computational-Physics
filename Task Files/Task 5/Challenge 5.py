88cf379cecaa84377a3f6a2f0c656c4afd7ebcfe
ArmouredApe <133009055+ArmouredApe@users.noreply.github.com>
2023-06-21 13:35:59 +0100

    Update Challenge 5.py
    I am 80% sure this works, it works for pluto at least

--- Task Files/Task 5/Challenge 5.py
+++ Task Files/Task 5/Challenge 5.py
@@ -10,15 +10,16 @@ w= []
 y= []
 k= []
 l= []
+
 #time /years, planet number
 ecc = float(Pd.object_data[8][2]) #eccentricity 
 p = float(Pd.object_data[8][6]) # orbital time
 
 def f(x):           
-    return (1-ecc*math.cos(x))**2    # Define function to integrate
+    return (2*math.pi/p)*(1+ecc*math.cos(x))**-2    # Define function to integrate
 
 # function, lower limit, upper limit, number of subintervals
-def integration(f, a, b, n,):
+def integration(f, a, b, n):
   h = (b - a) / n
   s = f(a) + f(b)
   for i in range(1, n, 2):
@@ -28,23 +29,24 @@ def integration(f, a, b, n,):
   return s * h / 3
 
 a = 0
-n = 10000
+n = 1000
 
 for t in range(0, 800):
     N = (t/p) # number of orbits
     b = 2*math.pi*N
     w.append(t)
-    result = integration(f, a, b, n)
+    result = ((integration(f, a, b, n)) *(p*((1-ecc**2)**(3/2) )*(1/(2*math.pi)) ))
     y.append(result)
-
+    
 for t in range(0, 800):
     ecc = 0
     N = (t/p) # number of orbits
     b = 2*math.pi*N
     k.append(t)
-    result = integration(f, a, b, n)
+    result = ((integration(f, a, b, n)) *(p*((1-ecc**2)**(3/2))*(1/(2*math.pi)) ))
     l.append(result)
+
+   
 plt.plot(w, y, "r")
 plt.plot(k, l, "b")
 plt.show()
-
