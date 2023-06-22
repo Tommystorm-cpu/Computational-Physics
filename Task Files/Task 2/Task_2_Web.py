7eaf9edc96eea1f676e6c42e3e4b2dfc1488b4de
Tommy <tommykdavey@gmail.com>
2023-06-22 17:52:16 +0100

    Website Update
    This is so cool

--- Task Files/Task 2/Task_2_Web.py
+++ Task Files/Task 2/Task_2_Web.py
@@ -2,28 +2,42 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 import PlanetData as Pd
+import pyscript as ps
 
-fig = plt.figure()
-ax = fig.add_subplot()
-ax.set_aspect('equal', adjustable='box')
+cool_number = 5
 
-for i in range(len(Pd.inner_planets)):
-    x_coords = []
-    y_coords = []
-    for theta in np.arange(0, (2*math.pi)+1, 0.1):
-        semi_major = float(Pd.inner_planets[i][1])
-        eccen = float(Pd.inner_planets[i][2])
-        radius = (semi_major*(1-(eccen*eccen)))/(1-eccen*math.cos(theta))
-        x = radius * math.cos(theta)
-        y = radius * math.sin(theta)
-        x_coords.append(x)
-        y_coords.append(y)
-    plt.plot(x_coords, y_coords, label=Pd.inner_planets[i][7])
 
-plt.plot(0, 0, marker="o", markersize=20, markerfacecolor="yellow")
-plt.title("Inner Solar System")
-plt.xlabel("X (AU)")
-plt.ylabel("Y (AU)")
-plt.legend(loc="upper right")
+def cool_function(input):
+    print(input)
 
-display(fig, target="graph-area", append=False)
+
+def plot_orbit(target, solar_type):
+    data = 0
+    if solar_type == "Inner":
+        data = Pd.inner_planets
+    elif solar_type == "Outer":
+        data = Pd.outer_planets
+    fig = plt.figure()
+    ax = fig.add_subplot()
+    ax.set_aspect('equal', adjustable='box')
+
+    for i in range(len(data)):
+        x_coords = []
+        y_coords = []
+        for theta in np.arange(0, (2*math.pi)+1, 0.1):
+            semi_major = float(data[i][1])
+            eccen = float(data[i][2])
+            radius = (semi_major*(1-(eccen*eccen)))/(1-eccen*math.cos(theta))
+            x = radius * math.cos(theta)
+            y = radius * math.sin(theta)
+            x_coords.append(x)
+            y_coords.append(y)
+        plt.plot(x_coords, y_coords, label=data[i][7])
+
+    plt.plot(0, 0, marker="o", markersize=20, markerfacecolor="yellow")
+    plt.title("Inner Solar System")
+    plt.xlabel("X (AU)")
+    plt.ylabel("Y (AU)")
+    plt.legend(loc="upper right")
+
+    ps.display(fig, target=target, append=False)
