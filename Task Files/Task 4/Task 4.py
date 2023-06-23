5a05643a10274c4d1679b3ac723657b3cb15dbce
Tommy <tommykdavey@gmail.com>
2023-06-23 21:59:36 +0100

    Task 6 Complete
    That was fast lol
    Spirographs look very very cool

--- Task Files/Task 4/Task 4.py
+++ Task Files/Task 4/Task 4.py
@@ -8,7 +8,7 @@ import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath("__file__")))))
 import PlanetData as Pd
 
-solar_system = Pd.outer_planets
+solar_system = Pd.solar_system
 
 title = ""
 
