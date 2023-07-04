e3f69edd709bc1f355158ae33134761baa8c1ec1
Tommy <tommykdavey@gmail.com>
2023-07-04 17:48:11 +0100

    Planet Lock on added + Very Bad Sun

--- 3D Scene Prototype/main4.js
+++ 3D Scene Prototype/main4.js
@@ -3,30 +3,32 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { solarSystem } from './planetData.js';
 import { get_angle } from './Theta_Function.js';
 
-let scene, camera, renderer, light, time, splines, planets;
+let scene, camera, controls, renderer, light, time, splines, planets, cameraTarget, fakeCamera;
 
 function init() {
+    cameraTarget = 0;
     time = 0;
 
     // Scene
     scene = new THREE.Scene();
 
     // Renderer
-    renderer = new THREE.WebGLRenderer({ antialias: true});
+    renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.shadowMap.enabled = true;
-    renderer.shadowMap.type = THREE.PCFSoftShadowMap; 
+    renderer.shadowMap.type = THREE.PCFSoftShadowMap;
     document.body.appendChild(renderer.domElement);
 
     // Camera
-    camera = new THREE.PerspectiveCamera( 
-        75, 
-        window.innerWidth / window.innerHeight, 
-        1, 
-        10000000 
+    camera = new THREE.PerspectiveCamera(
+        75,
+        window.innerWidth / window.innerHeight,
+        1,
+        10000000
     );
+    camera.position.y = 1000;
     camera.position.z = 1000;
-    
+
     // Light and Shadows
     light = new THREE.PointLight(0xffffff, 3);
     light.position.set(0, 0, 0);
@@ -35,7 +37,7 @@ function init() {
     light.shadow.mapSize.height = 1024;
     light.shadow.radius = 50;
     scene.add(light);
-    
+
     light.shadow.camera.far = 1000000;
 
     const ambLight = new THREE.AmbientLight(0xffffff, 0.1)
@@ -47,16 +49,47 @@ function init() {
 
     // Skybox
     const loader = new THREE.CubeTextureLoader();
-    loader.setPath( 'Skybox/' );
-    const textureCube = loader.load( [ 'right.png', 'left.png', 'top.png', 'bottom.png', 'front.png', 'back.png' ] );
+    loader.setPath('Skybox/');
+    const textureCube = loader.load(['right.png', 'left.png', 'top.png', 'bottom.png', 'front.png', 'back.png']);
     scene.background = textureCube;
 
     // Controls
-    const controls = new OrbitControls(camera, renderer.domElement );
+    controls = new OrbitControls(camera, renderer.domElement);
+
+    fakeCamera = camera.clone();
+    controls = new OrbitControls(fakeCamera, renderer.domElement);
+    // controls.enablePan = false;
+
+
+    document.addEventListener('keyup', (event) => {
+
+        const key = event.key;
+
+        if (isFinite(key)) {
+            if (key === "0") {
+                if (cameraTarget == 0) {
+                    controls.reset();
+                } else {
+                    cameraTarget.remove(camera);
+                    fakeCamera.position.add(cameraTarget.position);
+                    cameraTarget = 0;
+                    controls.enablePan = true;
+                };
+            } else {
+                controls.reset();
+                cameraTarget = planets[parseInt(key) - 1][1];
+                cameraTarget.add(camera);
+                const tempRadius = planets[parseInt(key) - 1][0][3] * 300;
+                const startPos = new THREE.Vector3(tempRadius, tempRadius, tempRadius);
+                fakeCamera.position.copy(startPos);
+                controls.enablePan = false;
+            };
+        }
+    });
 }
 
 function generatePlanet(planet) {
-    const radius = planet[3] * 100;
+    const radius = planet[3] * 200;
     const textureType = planet[6];
     const clouds = planet[7];
 
@@ -72,19 +105,26 @@ function generatePlanet(planet) {
 
     const variant = randInt(1, 10);
     const textureLoader = new THREE.TextureLoader();
-    const materialNormalMap = new THREE.MeshPhongMaterial( {
+    const materialNormalMap = new THREE.MeshPhongMaterial({
         map: textureLoader.load(mapPath),
         normalMap: textureLoader.load(normalPath)
-    } );
+    });
     materialNormalMap.map.colorSpace = THREE.SRGBColorSpace;
 
     // Planet Mesh
     const geometry = new THREE.SphereGeometry(radius, 500, 250);
-    const meshPlanet = new THREE.Mesh( geometry, materialNormalMap);
+    const meshPlanet = new THREE.Mesh(geometry, materialNormalMap);
     meshPlanet.castShadow = true;
     meshPlanet.receiveShadow = true;
-    scene.add(meshPlanet);
-    result.push(meshPlanet);
+
+    // Point Geometry (for camera locking)
+    const pointgeo = new THREE.Points();
+    const group = new THREE.Group();
+    group.add(meshPlanet);
+    group.add(pointgeo);
+
+    scene.add(group);
+    result.push(group);
 
     if (clouds == 1) {
         // Clouds Texture
@@ -94,16 +134,16 @@ function generatePlanet(planet) {
         } else {
             cloudPath = './textures/Martian/Martian Clouds ('.concat(variant).concat(').png');
         }
-        
-        const materialClouds = new THREE.MeshLambertMaterial( {
+
+        const materialClouds = new THREE.MeshLambertMaterial({
             map: textureLoader.load(cloudPath),
             transparent: true
-        } );
+        });
         materialClouds.map.colorSpace = THREE.SRGBColorSpace;
 
         // Clouds Mesh
-        const meshClouds = new THREE.Mesh( geometry, materialClouds );
-        meshClouds.scale.set( 1.005, 1.005, 1.005);
+        const meshClouds = new THREE.Mesh(geometry, materialClouds);
+        meshClouds.scale.set(1.005, 1.005, 1.005);
         meshClouds.receiveShadow = true;
         scene.add(meshClouds);
         result.push(meshClouds);
@@ -115,13 +155,13 @@ function generatePlanet(planet) {
 function generateOrbit(semi_major, eccen, inclination) {
     // Generate spline
     const generatedPoints = [];
-    const scalar = 1000;
+    const scalar = 2000;
 
-    for (var angle = 0; angle < 2*Math.PI; angle+=0.01){
-        const radius = (semi_major*(1-(Math.pow(eccen, 2))))/(1-eccen*Math.cos(angle))
+    for (var angle = 0; angle < 2 * Math.PI; angle += 0.01) {
+        const radius = (semi_major * (1 - (Math.pow(eccen, 2)))) / (1 - eccen * Math.cos(angle))
         const xPos = radius * Math.sin(angle) * scalar;
         const yPos = radius * Math.cos(angle) * scalar;
-        const zPos = xPos * Math.tan(inclination * (Math.PI/180));
+        const zPos = xPos * Math.tan(inclination * (Math.PI / 180));
         const vector = new THREE.Vector3(xPos, zPos, yPos);
         generatedPoints.push(vector);
     }
@@ -129,19 +169,37 @@ function generateOrbit(semi_major, eccen, inclination) {
     const spline = new THREE.CatmullRomCurve3(generatedPoints);
 
     // Draw Spline
-    const points = spline.getPoints(150);
+    const points = spline.getPoints(300);
     const splineGeometry = new THREE.BufferGeometry().setFromPoints(points);
-    const material = new THREE.LineBasicMaterial({color: 0xff0000});
+    const material = new THREE.LineBasicMaterial({ color: 0xff0000 });
     const curveObject = new THREE.Line(splineGeometry, material);
     scene.add(curveObject);
 
     return spline;
 }
 
-function positionObject(curve, object, orbitTime, planetData){
+function generateStar(starData) {
+    const radius = starData[0] * 3;
+    const textureType = starData[2];
+    let mapPath = "";
+    if (textureType == "Sun") {
+        mapPath = "./textures/Solar System/Sun Map.jpg"
+    }
+
+    const textureLoader = new THREE.TextureLoader();
+    const materialMap = new THREE.MeshPhongMaterial({
+        map: textureLoader.load(mapPath),
+    });
+
+    const geometry = new THREE.SphereGeometry(radius, 500, 250);
+    const starMesh = new THREE.Mesh(geometry, materialMap);
+    scene.add(starMesh);
+
+}
+
+function positionObject(curve, object, orbitTime, planetData) {
     const period = planetData[5];
     const eccen = planetData[1];
-    //const angle = (2 * Math.PI *  orbitTime) / period;
     const angle = get_angle(eccen, period, orbitTime);
     let pos = angle / (2 * Math.PI);
     pos = pos - Math.floor(pos)
@@ -155,17 +213,20 @@ function animate() {
     for (var i = 0; i < planets.length; i++) {
 
         // Rotate planet mesh
-        planets[i][1].rotateY(planets[i][0][4] / 100);
+        planets[i][1].children[0].rotateY(planets[i][0][4] / 100);
         // Move planet mesh (orbit path, planet mesh, time, planet data)
         positionObject(splines[i], planets[i][1], time, planets[i][0]);
-        
+
         // Same as above for clouds
-        if (planets[i].length == 3)  {
+        if (planets[i].length == 3) {
             planets[i][2].rotateY((planets[i][0][4] / 100) * 1.5);
             positionObject(splines[i], planets[i][2], time, planets[i][0]);
         }
     }
     time += 0.001;
+
+    camera.copy(fakeCamera);
+    controls.update();
     renderer.render(scene, camera);
 }
 
@@ -175,7 +236,7 @@ function onWindowResize() {
     renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
-function randInt(min, max) { 
+function randInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
@@ -187,8 +248,12 @@ init();
 splines = []
 planets = []
 for (const [key, value] of Object.entries(solarSystem)) {
-  splines.push(generateOrbit(value[0], value[1], value[2]));
-  planets.push(generatePlanet(value))
+    if (key != Object.keys(solarSystem)[0]) {
+        splines.push(generateOrbit(value[0], value[1], value[2]));
+        planets.push(generatePlanet(value));
+    } else {
+        generateStar(value);
+    }
 }
 
 
