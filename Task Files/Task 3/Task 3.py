7a3876ea134079f05d129aa1d9c550aab46c6118
Tommy <tommykdavey@gmail.com>
2023-08-03 16:33:38 +0100

    Lots of videos

--- Task Files/Task 3/Task 3.py
+++ Task Files/Task 3/Task 3.py
@@ -126,16 +126,15 @@ def create_video(planet_system_name):
     plt.legend(loc="upper right")
     plt.title(title)
 
-    #writervideo = animation.FFMpegWriter(fps=60)
-    #anim.save(f"{planet_system_name}.mp4", writer = writervideo, dpi=900)
+    writervideo = animation.FFMpegWriter(fps=60)
+    anim.save(f"{planet_system_name} 2D.mp4", writer = writervideo, dpi=200)
 
-    plt.show()
+    #plt.show()
 
-"""
+
 for planet_system_name in Pd.system_list:
     if planet_system_name != "All":
         create_video(planet_system_name)
-"""
 
 
-create_video("All")
+#create_video("Inner Solar")
