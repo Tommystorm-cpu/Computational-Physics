dc8cc1b00d01e6ad58d169bbb14b5448332f885c
ArmouredApe <acarnold1@outlook.com>
2023-08-03 18:13:55 +0100

    task 4 with video

--- Task Files/Task 4/Task 4.py
+++ Task Files/Task 4/Task 4.py
@@ -4,19 +4,18 @@ import math
 import os.path
 
 from matplotlib.animation import FuncAnimation
+import mpl_toolkits.mplot3d.art3d as art3d
+import matplotlib.animation as animation
 import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath("__file__")))))
 import PlanetData as Pd
 import Theta_Function
 
-system_name = "Inner Solar"
-solar_system = Pd.system_list[system_name]
-
-title = system_name + " System"
+plt.rcParams['animation.ffmpeg_path'] = r'C:\\Users\\Alex Arnold\\Desktop\\ffmpeg\\bin\\ffmpeg.exe'
 
 
 class Planet:
-    def __init__(self, data, index):
+    def __init__(self, data, index, ax, max_period, max_semi_major):
         self.period = float(data[6])
         self.semi_major = float(data[1])
         self.eccen = float(data[2])
@@ -29,16 +28,24 @@ class Planet:
         self.index = index
         self.point = 0
         self.data = data
-        self.time_scale = float(solar_system[0][6]) / 100
+        self.time_scale = max_period / 1000
         self.colour = (0, 0, 0)
+        self.xArray = []
+        self.yArray = []
+        self.zArray = []
+
+        self.plot_orbit()
+        self.create_animation_list()
+
+        self.point, = ax.plot(self.xArray[0], self.yArray[0], self.zArray[0], 'o', markersize=7, color = self.colour)
 
     def plot_orbit(self):
         x_coords = []
         y_coords = []
         z_coords = []
         for theta in np.arange(0, (2 * math.pi) + 1, 0.1):
-            semi_major = float(solar_system[self.index][1])
-            eccen = float(solar_system[self.index][2])
+            semi_major = float(self.data[1])
+            eccen = float(self.data[2])
             radius = (semi_major * (1 - (eccen * eccen))) / (1 - eccen * math.cos(theta))
             x = radius * math.cos(theta)
             y = radius * math.sin(theta)
@@ -50,54 +57,84 @@ class Planet:
         line = plt.plot(x_coords, y_coords, z_coords,  label=self.name)
         self.colour = line[0].get_color()
 
-    def init_func(self):
-        self.point, = ax.plot(0, 0, 0, 'o', markersize=7, color=self.colour)
-
-    def step_function(self, time):
-        time = time * self.time_scale
-        # theta = (2 * math.pi * time) / self.period
-        theta = Theta_Function.get_angle(self.data, time)
-        r = (self.semi_major*(1-self.eccen**2)) / (1 - (self.eccen * math.cos(theta)))
-        x = r * math.cos(theta)
-        y = r * math.sin(theta)
-        z = x * math.sin(self.incline)
-        x = x * math.cos(self.incline)
-
-        self.point.set_data_3d(x, y, z)
-        return self.point
+    def create_animation_list(self):
+        num_of_orbits = 2
+        for time_passed in np.arange(0, num_of_orbits *1000, 1):
+            time = time_passed *self.time_scale
+            theta = Theta_Function.get_angle(self.data, time)
+            r = (self.semi_major*(1-self.eccen**2)) / (1 - (self.eccen * math.cos(theta)))
+            x = r * math.cos(theta)
+            y = r * math.sin(theta)
+            z = x * math.sin(self.incline)
+            x = x * math.cos(self.incline)
+            self.xArray.append(x)
+            self.yArray.append(y)
+            self.zArray.append(z)
 
 
-plt.style.use('dark_background')
-fig = plt.figure()
-ax = fig.add_subplot(projection="3d")
+def create_video(planet_system_name):
+    def animation_init():
+        output = []
+        for planet in planet_list:
+            planet.point.set_data_3d([planet.xArray[0]], [planet.yArray[0]], [planet.zArray[0]])
+            output.append(planet.point)
+        return output
 
-ax.set_aspect('equal')
-limit = float(solar_system[len(solar_system)-1][1])
+    def animate_func(i):
+        output = []
+        for planet in planet_list:
+            planet.point.set_data_3d([planet.xArray[i]], [planet.yArray[i]], [planet.zArray[i]])
+            output.append(planet.point)
+        return output
 
-ax.set_xlim(-limit, limit)
-ax.set_ylim(-limit, limit)
-ax.set_zlim(-limit, limit)
+    plt.style.use('dark_background')
+    fig = plt.figure()
+    ax = fig.add_subplot(projection="3d")
+    title = planet_system_name + " System"
+    planet_system = Pd.system_list[planet_system_name]
 
+    ax.set_aspect('equal')
+    limit = float(planet_system[len(planet_system)-1][1])
 
-ax.set_xlabel("X (AU)")
-ax.set_ylabel("Y (AU)")
-ax.set_zlabel("Z (AU)")
+    ax.set_xlim(-limit, limit)
+    ax.set_ylim(-limit, limit)
+    ax.set_zlim(-limit, limit)
 
-ax.xaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
-ax.yaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
-ax.zaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
+    ax.set_xlabel("X (AU)")
+    ax.set_ylabel("Y (AU)")
+    ax.set_zlabel("Z (AU)")
 
-ax.grid(False)
+    ax.xaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
+    ax.yaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
+    ax.zaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
 
-plt.plot(0, 0, 0, marker="o", markersize=10, markerfacecolor="yellow", markeredgecolor=(0.6, 0.6, 0))
+    ax.grid(False)
 
-ani_list = []
-for count, planet in enumerate(solar_system):
-    plan_class = Planet(solar_system[planet], count)
-    plan_class.plot_orbit()
-    ani_list.append(FuncAnimation(fig, plan_class.step_function, repeat=True, interval=1, init_func=plan_class.init_func()))
+    plt.plot(0, 0, 0, marker="o", markersize=10, markerfacecolor="yellow", markeredgecolor=(0.6, 0.6, 0))
 
-plt.legend(loc="upper right")
-plt.title(title)
+    planet_list = []
 
-plt.show()
+    max_period = 0
+    max_semi_major = 0
+    for planet in planet_system:
+        planet = planet_system[planet]
+        if float(planet[6]) > max_period:
+            max_period = float(planet[6])
+        if float(planet[1]) > max_semi_major:
+            max_semi_major = float(planet[1])
+
+    for count, planet in enumerate(planet_system):
+        plan_class = Planet(planet_system[planet], count, ax, max_period, max_semi_major)
+        planet_list.append(plan_class)
+
+    anim = FuncAnimation(fig, animate_func, init_func=animation_init, frames=len(planet_list[0].xArray), interval=0.1, blit=False, repeat=True)
+
+    plt.legend(loc="upper right")
+    plt.title(title)
+
+    #writervideo = animation.FFMpegWriter(fps=60)
+    #anim.save(f"{planet_system_name}.mp4", writer=writervideo, dpi=200)
+
+    plt.show()
+
+create_video("Inner Solar")
