23318307c8acfb44ca8cca988cef24a0e68d5ff2
Tommy <tommykdavey@gmail.com>
2023-08-11 21:10:09 +0100

    Solar System Viewer comments

--- Planet 3D Sim/SolarSystemViewer.js
+++ Planet 3D Sim/SolarSystemViewer.js
@@ -7,8 +7,10 @@ import { Sun } from './Sun.js';
 import { system_list, system_scales, sun_radii } from './PlanetData.js';
 
 
+// Main class to handle nearly all rendering and logic
 export class SolarSystemViewer {
     constructor () {
+        // Initialize all the variables
         this.cameraTarget = 0;
         this.time = 0;
         this.timeStep = 0.5;
@@ -32,12 +34,14 @@ export class SolarSystemViewer {
         this.endTime = [];
         this.smallestOrbit = 100000;
 
+        // Call initialisation functions
         this.init3d();
         this.createLabelRenderer();
         this.createPlanets();
         this.createControls();
         this.createStar();
         
+        // Create input handler
         this.inputHandler = new InputHandler(this);
 
         this.setSunLock(false);
@@ -62,15 +66,17 @@ export class SolarSystemViewer {
             0.1,
             250000000000
         );
-
-        this.createLights();
         
+        // Load lights and skybox
+        this.createLights();
         this.loadSkybox();
-
+        
+        // Resizing event
         window.addEventListener('resize', () => this.onWindowResize(), false);
     }
 
     onWindowResize () {
+        // Resizing script
         this.fakeCamera.aspect = window.innerWidth / window.innerHeight;
         this.fakeCamera.updateProjectionMatrix();
 
@@ -86,6 +92,7 @@ export class SolarSystemViewer {
     }
 
     toggleLabels (labelBool) {
+        // Toggles labels for each planet
         this.inputHandler.labelRadial.checked = labelBool;
         this.planets.forEach(planet => {
             planet.label.visible = labelBool;
@@ -93,6 +100,7 @@ export class SolarSystemViewer {
     }
 
     toggleAccurate (accurate) {
+        // Toggles accurate scaling mode
         this.accurateScale = accurate;
         this.planets.forEach(planet => {
             planet.toggleAccurateOrbit(accurate);
@@ -104,7 +112,9 @@ export class SolarSystemViewer {
     }
 
     loadSkybox () {
-        this.planetTextures++;
+        this.planetTextures++; // for the loading screen
+
+        // Loads textures
         const loader = new THREE.CubeTextureLoader();
         loader.setPath('Skybox/');
         const textureCube = loader.load(
@@ -113,10 +123,12 @@ export class SolarSystemViewer {
             null, 
             () => this.solarSystemViewer.planetTextureLoad()
         );
+        // Sets skybox
         this.scene.background = textureCube;
     }
 
     createLabelRenderer () {
+        // Creates the label renderer to handle labels
         const labelRenderer = new CSS2DRenderer();
         labelRenderer.setSize( window.innerWidth, window.innerHeight );
         labelRenderer.domElement.style.position = 'absolute';
@@ -126,6 +138,7 @@ export class SolarSystemViewer {
     }
 
     createLights () {
+        // Creates the main light in the scene
         const light = new THREE.PointLight(0xffffff, 3);
         light.position.set(0, 0, 0);
         light.castShadow = true;
@@ -133,7 +146,7 @@ export class SolarSystemViewer {
         light.shadow.mapSize.height = 1024;
         this.scene.add(light);
 
-        light.shadow.camera.far = 250000000000;
+        light.shadow.camera.far = 250000000000; // This has to be really high. It causes some artifacts unfortunately.
         light.shadow.camera.near = 0.1;
 
         this.scene.add(new THREE.AmbientLight(0xffffff, 0.1));
@@ -141,6 +154,7 @@ export class SolarSystemViewer {
     }
 
     createControls () {
+        // Creates a fake camera to allow orbit controls when locked onto a planet
         this.fakeCamera = this.camera.clone();
         this.controls = new OrbitControls(this.fakeCamera, this.labelRenderer.domElement);
 
@@ -149,22 +163,8 @@ export class SolarSystemViewer {
     }
 
     createPlanets () {
+        // Creates all the orbits
         const planets = this.planets;
-        /*
-        planets.set(1, new Planet(this, "Mercury", 0.387, 0.21, 7, 0.383, 58.646, 0.243, "Mercury", 0, 0, 0xd10000));
-        planets.set(2, new Planet(this, "Venus", 0.723, 0.01, 3.39, 0.949, 243.018,  0.615, "Venus", 0, 0, 0xd17300));
-        planets.set(3, new Planet(this, "Earth", 1, 0.02, 0, 1, 0.997, 1, "Earth", 1, 0, 0x2ad100));
-        planets.set(4, new Planet(this, "Mars", 1.523, 0.09, 1.85, 0.533, 1.026, 1.881, "Mars", 0, 0, 0x00d1ca));
-        planets.set(5, new Planet(this, "Jupiter", 5.202, 0.05, 1.31, 11.209, 0.413, 11.861, "Jupiter", 0, 0, 0x005bd1));
-        planets.set(6, new Planet(this, "Saturn", 9.576, 0.06, 2.49, 9.449, 0.444, 29.628, "Saturn", 0, 0, 0x1500d1));
-        planets.set(7, new Planet(this, "Uranus", 19.293, 0.05, 0.77, 4.007, 0.718, 84.747, "Uranus", 0, 0, 0x6f00d1));
-        planets.set(8, new Planet(this, "Neptune", 30.246, 0.01, 1.77, 3.883, 0.671, 166.344, "Neptune", 0, 0, 0xd100c3));
-        planets.set(9, new Planet(this, "Pluto", 39.509, 0.25, 17.5, 0.187, 6.387, 248.348, "Pluto", 0, 0, 0xd10046));
-        */
-
-        // Mass in Earth masses, semi-major axis, eccentricity, orbital inclination/degrees, Radius in earth radii, Rotational period/days, orbital period/years, planet name
-
-        // (solarSystemViewer, name, semiMajor, eccen, inclination, radius, rotatePeriod, orbitPeriod, textureType, cloudType, axisTilt, orbitColour)
         const colourList = [0xd10000, 0xd17300, 0x2ad100, 0x00d1ca, 0x005bd1, 0x1500d1, 0x6f00d1, 0xd100c3, 0xd10046];
 
         this.inaccurateScalar = system_scales[this.systemName];
@@ -173,10 +173,13 @@ export class SolarSystemViewer {
         let index = 0;
         for (const planetIndex in systemPlanets) {
             const planet = systemPlanets[planetIndex];
+            // (solarSystemViewer, name, semiMajor, eccen, inclination, radius, rotatePeriod, orbitPeriod, textureType, cloudType, axisTilt, orbitColour)
             planets.set(index, new Planet(this, planet[7], planet[1], planet[2], planet[3], planet[4], planet[5], planet[6], planet[8], planet[9], 0, colourList[index]));
             index ++;
         }
+        
 
+        // Grabs the smallest orbit in the system
         this.smallestOrbit = 100000;
         planets.forEach(planet => {
             this.scene.add(planet.group);
@@ -193,8 +196,10 @@ export class SolarSystemViewer {
     }
 
     planetTextureLoad () {
+        // Called when a planet texture is loaded
         this.planetTextures--;
 
+        // Destroys loading screen
         if (this.planetTextures == 0) {
             this.loaded = true;
             document.getElementById("loadingRing").style.display = "none";
@@ -215,6 +220,7 @@ export class SolarSystemViewer {
     }
 
     createStar () {
+        // Creates the sun
         const radius = sun_radii[this.systemName];
         this.star = new Sun("Sun", radius, "Sun", this);
         this.scene.add(this.star.mesh);
@@ -223,9 +229,11 @@ export class SolarSystemViewer {
     }
 
     lockOn (lockPlanet) {
+        // Locks on to planet. 0 = lock onto sun.
         if (lockPlanet != 0) {
             this.inputHandler.focusDropDown.value = lockPlanet.name;
             if (this.cameraTarget != lockPlanet) {
+                // Positions camera
                 this.controls.reset();
                 this.cameraTarget = lockPlanet;
                 this.cameraTarget.group.add(this.camera);
@@ -237,6 +245,7 @@ export class SolarSystemViewer {
                 this.setSunLock(this.sunLock);
             };
         } else {
+            // Locks onto sun.
             let text = "";
             if (["Inner Solar", "Outer Solar", "Solar"].includes(this.systemName)) {
                 text = "Sun";
@@ -260,6 +269,7 @@ export class SolarSystemViewer {
     }
 
     setSunLock (sunBool) {
+        // Sets sunlock and updates variables
         this.sunLock = sunBool;
         this.inputHandler.sunRadial.checked = sunBool;
 
@@ -271,6 +281,7 @@ export class SolarSystemViewer {
     }
 
     updateSunLock () {
+        // Positions camera according to sunlock
         if (this.sunLock) {
             let cameraPos = new THREE.Vector3();
             const vertOffest = new THREE.Vector3(0, this.cameraRadii * this.inaccurateScalar * 1.5, 0);
@@ -283,28 +294,32 @@ export class SolarSystemViewer {
     }
 
     render () {
-        //this.time += this.timeStep/50;
+        // Main render loop
+
+        // Calculates fps
         this.endTime.push(Date.now());
-        //console.log(this.endTime);
         let fps = 1000 / (this.endTime[this.endTime.length - 1] - this.endTime[this.endTime.length - 2]);
         if (!fps) {
             fps = 60;
         }
-        // 1 earth orbit per second (1 year/second)
         this.time += this.timeStep/fps;
-
+        
+        // Doesn't render if not loaded
         requestAnimationFrame(() => this.render());
         if (!this.loaded) {
             return;
         }
 
+        // Fake camera shenanigans
         this.camera.copy(this.fakeCamera);
         this.controls.update();
 
+        // Renders each planet
         this.planets.forEach(planet => {
             planet.render(this.time, this.timeStep/fps);
         });
 
+        // Updates camera and star
         this.updateSunLock();
         if (this.accurateScale) {
             this.star.changeSpriteSizeAccurate();
@@ -312,9 +327,11 @@ export class SolarSystemViewer {
         this.star.updateSunVisibility();
         this.star.mesh.rotateY(0.01 * this.timeStep);
 
+        // Renders scene and labels
         this.renderer.render(this.scene, this.camera);
         this.labelRenderer.render(this.scene, this.camera);
 
+        // Updates time UI
         this.inputHandler.updateTimeLabel();
     }
 }
